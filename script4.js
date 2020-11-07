454d41fa7fcc9fa3e7a4952c0195a87b016d01b8
ania <askrzyszton@gmail.com>
2020-11-07 21:48:41 +0100

    przeniesienie plików

--- script4.js
+++
@@ -1,9 +0,0 @@
-//Napisz funkcję, która zwraca słowo something połączony ze spacją „ ”.
-
-function GiveMeSth(){
-    let sth = "something ";
-    let text = prompt('wpisz tekst');
-    return sth + text;
-}
-
-console.log(GiveMeSth());
---
+++ sprawdzone/script4.js
@@ -0,0 +1,9 @@
+//Napisz funkcję, która zwraca słowo something połączony ze spacją „ ”.
+
+function GiveMeSth(){
+    let sth = "something ";
+    let text = prompt('wpisz tekst');
+    return sth + text;
+}
+
+console.log(GiveMeSth());
