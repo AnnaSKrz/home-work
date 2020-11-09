ad5b2207dce67e69a9f3ddbc3c7dbe1e87116c50
ania <askrzyszton@gmail.com>
2020-11-09 10:19:42 +0100

    wykonane zadanie nr 9

--- script9.js
+++ script9.js
@@ -1,21 +1,27 @@
 // Mając podaną wartość pH, zwróć, czy jest to wartość zasadowa (większa niż 7), „kwaśna” (mniejsza niż 7) czy obojętna (7). Zwróć nieprawidłowy, jeśli podana wartość jest mniejsza niż 0 lub większa niż 14. Proszę wykorzystać instrukcję switch.
 
-let ph = 6;
+let ph = 8.9;
 
 function phName(ph){
     var result;
-    switch(ph){
-        case 8:
-            result = "zasadowa";
+    switch(true){
+        case ph > 7 && ph < 14:
+            result = "zasadowe";
             break;
-        case 6:
-            result = "kwaśna";
+        case ph < 7 && ph > 0 :
+            result = "kwaśne";
             break;
+        case ph == 7:
+            result = "obojętne";
+            break;
+        case ph < 0:
+        case ph > 14:
+            result = "wartość nieprawidłowa";
         default:
             result = "nieprawidłowa wartość";
             break;
 }
-return result;
+return ("twoje ph (" + ph + ") to ph " + result);
 }
 
 console.log(phName(ph));
