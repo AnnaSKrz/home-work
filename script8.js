a935db59b21f845fab6d5021e9280577dc41b5a4
ania <askrzyszton@gmail.com>
2020-11-10 23:58:40 +0100

    wykonane zadanie 8

--- script8.js
+++ script8.js
@@ -1,3 +1,15 @@
 // Napisz funkcję, która przyjmuje obiekt jako parametr, ile stron może wydrukować każdy kolor atramentu. Podaj maksymalną liczbę stron, które drukarka może wydrukować, zanim skończy się którykolwiek z kolorów.
 
 //nie wiem jak zrobić
+
+
+let printColor = {
+    cyan : 3,
+    magenta : 12,
+    yellow: 100}
+
+    function inkLevels(printColor){
+        return ("(" + printColor.cyan + "," + printColor.magenta + "," + printColor.yellow + ") -> " + Math.min(printColor["cyan"],printColor["magenta"],printColor["yellow"]));
+    }
+
+console.log(inkLevels(printColor));
